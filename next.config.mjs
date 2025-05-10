96cba2dc0dba6dfa7d77f5734424d2725e112652
tharushaudana <tharushaudana519@gmail.com>
2025-05-10 08:46:42 +0530

    eslint disabled

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    // watchOptions: {
+    //     ignored: [
+    //         '**/C:/Users/ASUS/Application Data/**', // Explicit ignore
+    //         '**/node_modules/**',                   // General ignore
+    //     ],
+    // }
 };
 
 export default nextConfig;
